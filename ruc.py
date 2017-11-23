70a02860f4dff49c5e328f604b1019dddaba62e1
mYang <myishh@qq.com>
2017-11-22 20:58:22 -0500

    revise ruc.py

--- ruc.py
+++ ruc.py
@@ -1,54 +1,52 @@
 # -*- coding:utf-8 -*-
-# This is a web crawler, which is used to collect job positions of java or python developer in the site rdjy.ruc.edu.cn(Renmin Univ of China)
+# This is a web crawler, which is used to collect job positions of java or python developer in the site of <br>rdjy.ruc.edu.cn(Renmin Univ of China)
 
 import requests
 import time
 from bs4 import BeautifulSoup
 
 def trade_spider(max_page):
-    f = open("cueb.md", "w")
+    f = open("ruc.md", "w")
     page = 1
     while page <= max_page:
         url = "http://rdjy.ruc.edu.cn/cms/employment/?page=" + str(page)
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "lxml")
-        for link in soup.find_all('a'):
-            title = link.string.strip()
-            #print(title)
-            href =  "http://jy.cueb.edu.cn" + link.get("href")
+        soup2 = soup.find("div", {"class":"list"})
+        for link in soup2.find_all('a'):
+            href =  "http://rdjy.ruc.edu.cn" + link.get("href")
             #print(href)
-            #print("")
-            #print(link.get("title"))
-            get_single_item_info(href, f, page)
+            get_single_company_info(href, f, page)
         page += 1
     f.close()
 
-def get_single_item_info(item_url, f, page):
+def get_single_company_info(item_url, f, page):
     source_code = requests.get(item_url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
-    for item in soup.find_all("span"):
+    soup2 = soup.find("div", {"class":"col-md-10 article "})
+    #print(type(soup2))
+    for item in soup2.find_all("span"):
         content = item.string
         if isinstance(content, str):
             if ("java" in content.lower()
                 or "python" in content.lower()
                 #or u'\u6570\u636E\u6316\u6398' in content.lower()
                 ):
-                print_company_info(soup, item_url, f, page)
+                print_company_info(soup2, item_url, f, page)
                 break
-                #print(isinstance(title.string, str))
-                #print(type(title))
-                #print(title)
-                #print("Company Name: \t" + title)
 
-def print_company_info(soup, url, f, page):
-    for item in soup.find_all("span", {"class":"pd10 w230"}):
-        print(item.string + " page(" + (str(page) + ")"))
-        f.write(item.string + "</br>")
-        break
+def print_company_info(soup2, url, f, page):
+    item =  soup2.find("h3")
+
+    #right here, if use 'item.string', the result will be 'none'!!
+    #remember 'item.string' only can be used in the smallest tag!!
+    print(item.text + " page(" + (str(page) + ")"))
+    f.write(item.text + "</br>")
     print(url + "\n")
     f.write(url + "</br>" + "</br>")
 
-cnt = int(input("Input an int to decide crwal how many pages: "))
+#cnt = int(input("Input an int to decide crwal how many pages: "))
+cnt = 30
 trade_spider(cnt)
